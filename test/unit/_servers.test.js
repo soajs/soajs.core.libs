0f672f11a457cae3e5c09118e7e294eda45c9017
antoinehage <me@antoinehage.com>
2017-02-22 11:47:30 -0500

    cleaned up unit test

--- test/unit/_servers.test.js
+++ test/unit/_servers.test.js
@@ -1,29 +1,15 @@
 "use strict";
 var assert = require('assert');
-//var shell = require('shelljs');
-//var sampleData = require("soajs.mongodb.data/modules/soajs");
 
 describe("importing sample data", function () {
-	it("do import", function (done) {
-		/*
-		shell.pushd(sampleData.dir);
-		shell.exec("chmod +x " + sampleData.shell, function (code) {
-			assert.equal(code, 0);
-			shell.exec(sampleData.shell, function (code) {
-				assert.equal(code, 0);
-				shell.popd();
-				done();
-			});
-		});
-		*/
-		done();
-	});
+    it("do import", function (done) {
 
-	after(function (done) {
-		setTimeout(function () {
-			console.log('test data imported.');
-			require("./lib.utils.test.js");
-			done();
-		}, 1000);
-	});
+        done();
+    });
+
+    after(function (done) {
+        console.log('test data imported.');
+        require("./lib.utils.test.js");
+        done();
+    });
 });
